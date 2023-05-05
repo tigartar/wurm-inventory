a1d197335a0e71103f1747bd165accd5d1635d27
tigartar <thijs7421@hotmail.com>
2023-05-06 02:45:51 +1200

    update

--- inventory_system/parse_and_import.py
+++ inventory_system/parse_and_import.py
@@ -1,8 +1,13 @@
-import re
+import sqlite3
 import requests
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app import Item
+import re
+
+def create_table():
+    conn = sqlite3.connect('inventory.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT)''')
+    conn.commit()
+    conn.close()
 
 def fetch_and_parse_java_file():
     url = "https://raw.githubusercontent.com/tigartar/testserver/main/server-src/server/items/ItemTemplateCreator.java"
@@ -13,19 +18,19 @@ def fetch_and_parse_java_file():
     item_data = re.findall(pattern, java_file, re.DOTALL)
 
     return item_data
-	
-def import_items_to_db(item_data):
-    engine = create_engine('sqlite:///inventory.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    for item_id, item_name in item_data:
-        item = Item(id=int(item_id), name=item_name)
-        session.add(item)
+def import_items(item_data):
+    conn = sqlite3.connect('inventory.db')
+    c = conn.cursor()
 
-    session.commit()
-    session.close()
+    for item in item_data:
+        item_id, item_name = item
+        c.execute('INSERT OR REPLACE INTO items (id, name) VALUES (?, ?)', (item_id, item_name))
 
-if __name__ == '__main__':
+    conn.commit()  # Add this line to commit the changes to the database
+    conn.close()
+
+if __name__ == "__main__":
+    create_table()
     item_data = fetch_and_parse_java_file()
-    import_items_to_db(item_data)
+    import_items(item_data)

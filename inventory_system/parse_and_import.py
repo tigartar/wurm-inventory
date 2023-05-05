418a7f3a9618d61e8aa634aa6934f598d52f5b01
tigartar <thijs7421@hotmail.com>
2023-05-06 05:27:58 +1200

    update

--- inventory_system/parse_and_import.py
+++ inventory_system/parse_and_import.py
@@ -1,60 +1,59 @@
 import os
-import sys
 import sqlite3
 import re
-import argparse
 
-def parse_item_data(file_paths):
-    item_data = []
-    template_id_pattern = r'\d+|\w+'
-    pattern = re.compile(r'createItemTemplate\(\s*(' + template_id_pattern + r')\s*,\s*"([^"]+)"\s*,\s*"([^"]+)"\s*,\s*"([^"]+)"\s*,\s*(\d+)\s*,\s*(\d+)\s*\);')
+def parse_java_files(import_folder):
+    item_data_list = []
 
-    for file_path in file_paths:
-        with open(file_path, 'r') as file:
-            content = file.read()
-            matches = pattern.findall(content)
-            item_data.extend(matches)
+    for filename in os.listdir(import_folder):
+        if filename.endswith('.java'):
+            with open(os.path.join(import_folder, filename), 'r') as file:
+                content = file.read()
+                item_data = extract_item_data(content)
+                if item_data:
+                    item_data_list.append(item_data)
+    return item_data_list
 
-    # Filter out any non-numeric templateId values
-    item_data = [item for item in item_data if item[0].isdigit()]
+def extract_item_data(content):
+    # You can modify the regex pattern to match the fields in your .java files
+    pattern = re.compile(r'final int templateId = (.*?);.*?final int size = (.*?);.*?final String name = (.*?);.*?final String plural = (.*?);.*?final String itemDescriptionSuperb = (.*?);.*?final String itemDescriptionNormal = (.*?);.*?final String itemDescriptionBad = (.*?);.*?final String itemDescriptionRotten = (.*?);.*?final String itemDescriptionLong = (.*?);.*?final short\[\] itemTypes = (.*?);.*?final short imageNumber = (.*?);.*?final short behaviourType = (.*?);.*?final int combatDamage = (.*?);.*?final long decayTime = (.*?);.*?final int centimetersX = (.*?);.*?final int centimetersY = (.*?);.*?final int centimetersZ = (.*?);.*?final int primarySkill = (.*?);.*?final byte\[\] bodySpaces = (.*?);.*?final String modelName = (.*?);.*?final float difficulty = (.*?);.*?final int weightGrams = (.*?);.*?final byte material = (.*?);.*?final int value = (.*?);.*?final boolean isPurchased = (.*?);', re.DOTALL)
+    match = re.search(pattern, content)
+    if match:
+        return match.groups()
+    return None
 
-    return item_data
-
-def create_inventory_db(item_data, output_file):
-    conn = sqlite3.connect(output_file)
+def create_database_and_table(instance_folder):
+    db_path = os.path.join(instance_folder, 'inventory.db')
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    cursor.execute('CREATE TABLE IF NOT EXISTS ITEMS (TEMPLATEID INT PRIMARY KEY, NAME TEXT, DESCRIPTION TEXT, MODELNAME TEXT, SIZE INT, WEIGHT REAL)')
-
-    for item in item_data:
-        template_id = int(item[0])
-        name = str(item[1])
-        description = str(item[2])
-        model_name = str(item[3])
-        size = int(item[4])
-        weight = float(item[5])
-
-        cursor.execute("INSERT INTO ITEMS (TEMPLATEID, NAME, DESCRIPTION, MODELNAME, SIZE, WEIGHT) VALUES (?, ?, ?, ?, ?, ?)",
-                    (template_id, name, description, model_name, size, weight))
-
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS items
+        (templateId INTEGER, size INTEGER, name TEXT, plural TEXT, itemDescriptionSuperb TEXT, itemDescriptionNormal TEXT, itemDescriptionBad TEXT, itemDescriptionRotten TEXT, itemDescriptionLong TEXT, itemTypes TEXT, imageNumber INTEGER, behaviourType INTEGER, combatDamage INTEGER, decayTime INTEGER, centimetersX INTEGER, centimetersY INTEGER, centimetersZ INTEGER, primarySkill INTEGER, bodySpaces TEXT, modelName TEXT, difficulty REAL, weightGrams INTEGER, material INTEGER, value INTEGER, isPurchased BOOLEAN)
+    """)
     conn.commit()
-    conn.close()
+
+    return conn, cursor
+
+def insert_item_data_to_database(conn, cursor, item_data_list):
+    for item_data in item_data_list:
+        cursor.execute("""
+            INSERT INTO items (templateId, size, name, plural, itemDescriptionSuperb, itemDescriptionNormal, itemDescriptionBad, itemDescriptionRotten, itemDescriptionLong, itemTypes, imageNumber, behaviourType, combatDamage, decayTime, centimetersX, centimetersY, centimetersZ, primarySkill, bodySpaces, modelName, difficulty, weightGrams, material, value, isPurchased)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, item_data)
+    conn.commit()
 
 def main():
-    parser = argparse.ArgumentParser(description='Parse and import Wurm item data.')
-    parser.add_argument('action', choices=['parse', 'import'], help='Action to perform: parse or import.')
-    parser.add_argument('file', help='File to parse or SQLite database file to import data into.')
-    args = parser.parse_args()
+    import_folder = 'import'
+    instance_folder = 'instance'
 
-    if args.action == 'parse':
-        file_paths = [args.file]
-        item_data = parse_item_data(file_paths)
-        for item in item_data:
-            print(item)
-    elif args.action == 'import':
-        output_file = args.file
-        item_data = parse_item_data(['Items.java'])
-        create_inventory_db(item_data, output_file)
+    item_data_list = parse_java_files(import_folder)
+
+    conn, cursor = create_database_and_table(instance_folder)
+
+    insert_item_data_to_database(conn, cursor, item_data_list)
+
+    conn.close()
 
 if __name__ == '__main__':
     main()
